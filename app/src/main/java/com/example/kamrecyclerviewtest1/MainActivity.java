bf1c49432cbea6af58f3ed5ac914f21e3460c0ef
km <35552602+catnap7777@users.noreply.github.com>
2023-01-17 23:53:40 -0600

    added a switch

--- app/src/main/java/com/example/kamrecyclerviewtest1/MainActivity.java
+++ app/src/main/java/com/example/kamrecyclerviewtest1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.kamrecyclerviewtest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView1 = findViewById(R.id.kamTextView1);
+        TextView textViewColor1 = findViewById(R.id.kamTextViewColors);
+        Switch switch1 = findViewById(R.id.kamSwitch1);
+
+
+        String intro = "This is really interesting";
+        textView1.setText(intro);
+        textView1.setBackgroundColor(Color.GREEN);
+
+
+
+
+
+
+
+
     }
 }
